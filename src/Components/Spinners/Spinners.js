9615ad95eaa883b60f272a1563e02e8c5b8dbc37
charlyls <carlosgbozzola@hotmail.com>
2022-04-29 19:02:17 -0300

    spinner change to pacman

--- src/Components/Spinners/Spinners.js
+++ src/Components/Spinners/Spinners.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-import PacmanLoader from "react-spinners/ClipLoader";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const Spinners = () => {
   let [loading, setLoading] = useState(true);
   let [color, setColor] = useState("#003d8f");
 
   return (
-    <div className="d-flex justify-content-center">
-      <PacmanLoader color={color} loading={loading} size={150} />
+    <div className="d-flex justify-content-center mt-5">
+      <PacmanLoader color={color} loading={loading} size={50} />
     </div>
   );
 };
