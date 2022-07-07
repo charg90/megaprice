9b1dbc4f4004daf2a6847e22a84481690571ba95
charlyls <carlosgbozzola@hotmail.com>
2022-07-07 19:33:59 -0300

    home backgroudn changes

--- src/Components/AnimatedRoutes/AnimatedRoutes.js
+++ src/Components/AnimatedRoutes/AnimatedRoutes.js
@@ -1,25 +1,22 @@
-import React, { lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { Suspense } from "react";
+
 import Productos from "../../Pages/Productos/Productos";
-import Spinners from "../Spinners/Spinners";
-const Home = lazy(() => import("../../Pages/Home/Home"));
-const Empresa = lazy(() => import("../../Pages/Empresa/Empresa"));
-const Contact = lazy(() => import("../../Pages/Contact/Contact"));
+
+import Home from "./../../Pages/Home/Home";
+import Empresa from "./../../Pages/Empresa/Empresa";
+import Contact from "./../../Pages/Contact/Contact";
 
 const AnimatedRoutes = () => {
   const location = useLocation();
   return (
     <AnimatePresence>
-      <Suspense fallback={<Spinners />}>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="empresa" element={<Empresa />} />
-          <Route path="productos" element={<Productos />} />
-          <Route path="contacto" element={<Contact />} />
-        </Routes>
-      </Suspense>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="empresa" element={<Empresa />} />
+        <Route path="productos" element={<Productos />} />
+        <Route path="contacto" element={<Contact />} />
+      </Routes>
     </AnimatePresence>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { Container, Carousel, Row, Col, Card, Image } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { motion } from "framer-motion";
 import { Helmet } from "react-helmet";
-import fondo from "./../../img/animals.jpg";
-import Pic1 from "./../../img/pexels-cottonbro-3661243.jpg";
-import Pic2 from "./../../img/pexels-markus-spiske-168866.jpg";
-import pic3 from "./../../img/pexels-mike-191360.jpg";
-import monopatin from "./../../img/112-monopatin.png";
-import cGigante from "./../../img/117-camion-gigante.png";
-import escarabajo from "./../../img/122-primer-escarabajo.png";
+
 import "./Home.css";
 const Home = () => {
   return (
@@ -35,7 +29,7 @@ const Home = () => {
       >
         <div className="d-flex ">
           <img
-            className=" w-100 img-responsive  "
+            className=" w-100 img-responsive image "
             src="https://img.freepik.com/free-vector/many-cute-animals-bamboo-forest_1308-32851.jpg?w=2000"
             alt="First slide"
             style={{ height: "48rem" }}
@@ -55,92 +49,6 @@ const Home = () => {
           </p>
         </div>
       </Container>
-      {/* <Carousel>
-          <Carousel.Item interval={2500}>
-            <img
-              className="d-block w-100 "
-              src={Pic1}
-              alt="First slide"
-              style={{ height: "20rem" }}
-              fluid
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1500}>
-            <img
-              className="d-block w-100"
-              src={Pic2}
-              alt="Second slide"
-              style={{ height: "20rem" }}
-            />
-          </Carousel.Item>
-          <Carousel.Item interval={1500}>
-            <img
-              className="d-block w-100"
-              src={pic3}
-              alt="Third slide"
-              style={{ height: "20rem" }}
-            />
-          </Carousel.Item>
-        </Carousel> */}
-      {/* <Container className="  mt-5 container__info d-flex justify-content-center align-items-center rounded my-5 ">
-          <Row>
-            <Col xs={6} md={6} lg={66} className="d-flex align-items-center ">
-              <i class="fa-solid fa-truck-fast fa-2xl  text-white"></i>
-              <p class="text-white text-center m-0">
-                Envios a toda la Argentina
-              </p>
-            </Col>
-            <Col xs={6} md={6} lg={6} className="d-flex align-items-center">
-              <i class="fa-solid fa-certificate fa-2xl   text-white "></i>
-              <p class="text-white text-center  m-0">
-                Productos Certificados IRAM
-              </p>
-            </Col>
-          </Row>
-        </Container> */}
-      {/* <Container>
-          <Row>
-            <Col>
-              <h1 className="text-center">Los mas vendidos</h1>
-            </Col>
-          </Row>
-          <Row className="d-flex justify-content-center ">
-            <Card
-              style={{ width: "10rem" }}
-              className="mx-1  producto-background"
-            >
-              <Card.Img variant="top" src={monopatin} alt="no hay imagen" />
-              <Card.Body className="d-flex flex-column ">
-                <Card.Title className="text">Monopatin</Card.Title>
-                <Card.Text className="text mt-auto">
-                  Articulo : 112 nro.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-
-            <Card
-              style={{ width: "10rem" }}
-              className="mx-1 producto-background"
-            >
-              <Card.Img variant="top" src={cGigante} alt="no hay imagen" />
-              <Card.Body>
-                <Card.Title className="text">Camion Gigante</Card.Title>
-                <Card.Text className="text">Articulo : 112 nro</Card.Text>
-              </Card.Body>
-            </Card>
-
-            <Card
-              style={{ width: "10rem" }}
-              className="mx-1 mt-1 producto-background"
-            >
-              <Card.Img variant="top" src={escarabajo} alt="no hay imagen" />
-              <Card.Body>
-                <Card.Title className="text">Primer Escarabajo</Card.Title>
-                <Card.Text className="text">Articulo : 112 nro </Card.Text>
-              </Card.Body>
-            </Card>
-          </Row>
-        </Container> */}
     </motion.div>
   );
 };
