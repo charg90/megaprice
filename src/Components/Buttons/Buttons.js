f02818c4449c36523a5632a3c30be57420f0106e
charlyls <carlosgbozzola@hotmail.com>
2022-07-28 19:26:39 -0300

    button fix productos

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -5,7 +5,7 @@ export const buttons = [
     description:
       "Esta linea contiene diversos modelos de juguetes que tienen por objetivo la recreacion, el desarrollo y la estimulacion de los aspectos sensorio-motriz,intelectual y de convivencia social",
     state: false,
-    styles: "mx-1 mt-3  btn-infantil  ",
+    styles: "mx-1 mt-3  btn-infantil btn-lg ",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const buttons = [
     description:
       "Desarrollan sus habilidades sociales, sensoriales, la motriz fina, la memoria,la imaginacion y la creatividad.Tambien mejora su capacidad de concentracion y atencion, su autoestima y autoconfianza.",
     state: false,
-    styles: "mx-1 mt-3 btn-didactos ",
+    styles: "mx-1 mt-3 btn-didactos btn-lg ",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const buttons = [
     description:
       "Diversos modelos de autos, aviones, trenes en tamaño reducido y en diversos colores para jugar y coleccionar",
     state: false,
-    styles: "mx-1 mt-3 btn-little ",
+    styles: "mx-1 mt-3 btn-little btn-lg ",
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const buttons = [
     description:
       "Para que bañarse sea mucho mas divertido. A traves del baño logran asociar ese rato de higiene con el juego y el entretenimiento",
     state: false,
-    styles: "mx-1 mt-3 btn-bañadera ",
+    styles: "mx-1 mt-3 btn-bañadera  btn-lg",
   },
   {
     id: 5,
@@ -37,6 +37,6 @@ export const buttons = [
     description:
       "Juguetes para jugar al aire libre, en el arenero o en la playa. A los niños/niñas  les encanta experimentar con los jueguetes en el agua y en la arena.Estos elementos  estimulan los sentidos, fomentan el crecimiento y el desarrollo y ayudan a la coordinacion motora",
     state: false,
-    styles: "mx-1 mt-3 btn-playa",
+    styles: "mx-1 mt-3 btn-playa btn-lg",
   },
 ];
