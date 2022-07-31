2eb9c94b713750e708e0bef8d5cb23f69f2d651d
charlyls <carlosgbozzola@hotmail.com>
2022-07-31 18:08:51 -0300

    validation for contactos

--- src/Services/EmailJs.js
+++ src/Services/EmailJs.js
@@ -3,6 +3,27 @@ import Swal from "sweetalert2";
 
 const sendEmail = (e) => {
   e.preventDefault();
+  const nombre = e.target.nombreApellido.value;
+  const email = e.target.email.value;
+  const empresa = e.target.empresa.value;
+  const pais = e.target.pais.value;
+  const ciudad = e.target.ciudad.value;
+  const telefono = e.target.telefono.value;
+  const motivo = e.target.motivo.value;
+  const comment = e.target.comment.value;
+
+  if (
+    (nombre, email, empresa, pais, ciudad, telefono, motivo, comment === "")
+  ) {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "faltan campos por completar",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+
   emailjs
     .sendForm(
       "service_fq6x04p",
@@ -11,7 +32,6 @@ const sendEmail = (e) => {
       "n4WX9Oyn3Wg_n7Fqh"
     )
     .then((response) => {
-      console.log(response);
       Swal.fire({
         position: "top-end",
         icon: "success",
