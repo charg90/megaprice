68a1d703a39672d181d0aa6813e4e2da6d2e5603
charlyls <carlosgbozzola@hotmail.com>
2022-08-09 19:00:07 -0300

    uninstalling dependecies

--- src/Pages/Productos/Producto.js
+++ src/Pages/Productos/Producto.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Col, Image, Modal } from "react-bootstrap";
 import { motion } from "framer-motion";
+
 import ReactGA from "react-ga4";
 import "./producto.css";
 
